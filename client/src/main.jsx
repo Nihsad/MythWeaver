1e847a333a324060a5c5fa6258c636d117b0378b
HaleighElkins <152942336+HaleighElkins@users.noreply.github.com>
2024-05-29 22:17:37 -0600

    fixed conflicts and tree element

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,19 +3,13 @@ import ReactDOM from "react-dom";
 
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 // import bootstrap or other css framework here if needed
-
-<<<<<<< HEAD
-import App from './App.jsx'
-import Tree from './components/Tree/index.jsx';
-=======
 import App from './App.jsx';
+import Tree from './components/Tree/index.jsx';
 import Layout from './components/Layout/Layout.jsx';
 import MythIndex from './components/MythIndex/MythIndex.jsx';
 import UserProfile from './components/UserProfile/UserProfile.jsx';
 import StoryIndex from './components/StoryIndex/StoryIndex.jsx';
-import TreeComponent from './components/Tree/TreeComponent.jsx';
 
->>>>>>> 601d6681b9f51f4a3569c39ca0d009c195687b0a
 // import necessary pages here which will be rendered as elements in the children for react router
 
 const router = createBrowserRouter([
@@ -28,9 +22,6 @@ const router = createBrowserRouter([
             {
                 // Added in element for TreeComponent - Haleigh
                 index: true,
-<<<<<<< HEAD
-                element: <Tree />
-=======
                 element: <MythIndex />
             }, {
                 path: '/user-profile',
@@ -38,14 +29,11 @@ const router = createBrowserRouter([
             }, {
                 path: '/story-index',
                 element: <StoryIndex />
-            }, {
-                element: <TreeComponent />
->>>>>>> 601d6681b9f51f4a3569c39ca0d009c195687b0a
             }, 
-            // {
-            //     path: '/some-path',
-            //     element: <SomeElement />
-            // }
+            {
+                path: '/author-tree',
+                element: <Tree />
+            }
         ]
     }
 ]);
