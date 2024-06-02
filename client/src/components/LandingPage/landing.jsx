24b1022820efe45b4698ff03c543bc03362cef41
HaleighElkins <152942336+HaleighElkins@users.noreply.github.com>
2024-06-02 17:52:49 -0600

    got scroll and staffs in position

--- client/src/components/LandingPage/landing.jsx
+++ client/src/components/LandingPage/landing.jsx
@@ -1,12 +1,12 @@
 // import React from 'react';
 // import { Link } from 'react-router-dom';
-// import ScrollImage from '../../../public/images/Scroll.png';
-// import Book from '../../../public/images/book.png';
-// import Bag from '../../../public/images/bag.png';
-// import CoinPurse from '../../../public/images/coin_purse.png';
-// import Map from '../../../public/images/map.png';
-// import UprightStaff from '../../../public/images/staff_upright.png';
-// import UpsideDownStaff from '../../../public/images/staff_upsidedown.png';
+// import ScrollImage from '/images/Scroll.png'; 
+// import Book from '/images/book.png'; 
+// import Bag from '/images/bag.png';
+// import CoinPurse from '/images/coin_purse.png';
+// import Map from '/images/map.png'; 
+// import UprightStaff from '/images/staff_upright.png'; 
+// import UpsideDownStaff from '/images/staff_upsidedown.png'; 
 
 // import './landing.css';
 
@@ -14,21 +14,21 @@
 //   return (
 //     <div className="landing-page">
 //       <div className="hero">
+//         <img src={ScrollImage} alt="Scroll" className="scroll-image" />
 //         <div className="container">
 //           <h1 className="title">MythWeaver</h1>
-//           <p>Slogan TBA</p>
+//           <p className="slogan">Craft Your Tale, Weave Your Myth</p>
 //           <Link to="/myth-index" className="cta-button">Start your Adventure</Link>
 //         </div>
 //       </div>
 //       <section className="images-section">
 //         <div className="container">
-//         <img src={ScrollImage} alt="Scroll" className="scroll-image" />
-//         <img src={Book} alt="Book" className="book-image" />
-//         <img src={Bag} alt="Bag" className="bag-image" />
-//         <img src={CoinPurse} alt="CoinPurse" className="coinpurse-image" />
-//         <img src={Map} alt="Map" className="map-image" />
-//         <img src={UprightStaff} alt="StaffUpright" className="uprightstaff-image" />
-//         <img src={UpsideDownStaff} alt="Upsidedownstaff" className="upsidedownstaff-image" />
+//           <img src={Book} alt="Book" className="book-image" />
+//           <img src={Bag} alt="Bag" className="bag-image" />
+//           <img src={CoinPurse} alt="CoinPurse" className="coinpurse-image" />
+//           <img src={Map} alt="Map" className="map-image" />
+//           <img src={UprightStaff} alt="StaffUpright" className="uprightstaff-image" />
+//           <img src={UpsideDownStaff} alt="Upsidedownstaff" className="upsidedownstaff-image" />
 //         </div>
 //       </section>
 //       <footer className="footer">
@@ -45,13 +45,13 @@
 
 import React from 'react';
 import { Link } from 'react-router-dom';
-import ScrollImage from '../../../public/images/Scroll.png'; 
-import Book from '../../../public/images/book.png'; 
-import Bag from '../../../public/images/bag.png';
-import CoinPurse from '../../../public/images/coin_purse.png';
-import Map from '../../../public/images/map.png'; 
-import UprightStaff from '../../../public/images/staff_upright.png'; 
-import UpsideDownStaff from '../../../public/images/staff_upsidedown.png'; 
+import ScrollImage from '/images/Scroll.png'; 
+import Book from '/images/book.png'; 
+import Bag from '/images/bag.png';
+import CoinPurse from '/images/coin_purse.png';
+import Map from '/images/map.png'; 
+import UprightStaff from '/images/staff_upright.png'; 
+import UpsideDownStaff from '/images/staff_upsidedown.png'; 
 
 import './landing.css';
 
@@ -59,21 +59,21 @@ function Landing() {
   return (
     <div className="landing-page">
       <div className="hero">
+        <img src={UpsideDownStaff} alt="Upsidedownstaff" className="staff-image upside-down" />
+        <img src={ScrollImage} alt="Scroll" className="scroll-image" />
+        <img src={UprightStaff} alt="StaffUpright" className="staff-image upright" />
         <div className="container">
           <h1 className="title">MythWeaver</h1>
-          <p>Slogan TBA</p>
+          <p className="slogan">Craft Your Tale, Weave Your Myth</p>
           <Link to="/myth-index" className="cta-button">Start your Adventure</Link>
         </div>
       </div>
       <section className="images-section">
         <div className="container">
-          <img src={ScrollImage} alt="Scroll" className="scroll-image" />
           <img src={Book} alt="Book" className="book-image" />
           <img src={Bag} alt="Bag" className="bag-image" />
           <img src={CoinPurse} alt="CoinPurse" className="coinpurse-image" />
           <img src={Map} alt="Map" className="map-image" />
-          <img src={UprightStaff} alt="StaffUpright" className="uprightstaff-image" />
-          <img src={UpsideDownStaff} alt="Upsidedownstaff" className="upsidedownstaff-image" />
         </div>
       </section>
       <footer className="footer">
