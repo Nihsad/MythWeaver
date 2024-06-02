5e599887b915c361984a8a5c33c02e707a1cc465
HaleighElkins <152942336+HaleighElkins@users.noreply.github.com>
2024-06-02 16:57:16 -0600

    trying images on page, just testing

--- client/src/components/LandingPage/landing.jsx
+++ client/src/components/LandingPage/landing.jsx
@@ -1,38 +1,13 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import './landing.css';
-
-// function Landing() {
-//   return (
-//     <div className="landing-page">
-//       <div className="hero">
-//         <div className="container">
-//           <h1 className="title">MythWeaver</h1>
-//           <p>Slogan TBA </p>
-//           <a href="#about" className="cta-button">Start your Adventure</a>
-//         </div>
-//       </div>
-//       <section className="images-section">
-//         <div className="container">
-//           <img src="/images/image1.jpg" alt="Image 1" className="image"/>
-//           <img src="/images/image2.jpg" alt="Image 2" className="image"/>
-//           <img src="/images/image3.jpg" alt="Image 3" className="image"/>
-//         </div>
-//       </section>
-//       <footer className="footer">
-//         <div className="container">
-//           <p>&copy; 2024 MythWeaver. All rights reserved.</p>
-//         </div>
-//       </footer>
-//     </div>
-//   );
-// }
-
-// export default Landing;
-
-
 import React from 'react';
 import { Link } from 'react-router-dom';
+import ScrollImage from '../../assets/images/Scroll.png';
+import Book from '../../assets/images/book.png';
+import Bag from '../../assets/images/bag.png';
+import CoinPurse from '../../assets/images/coin_purse.png';
+import Map from '../../assets/images/map.png';
+import UprightStaff from '../../assets/images/staff_upright.png';
+import UpsideDownStaff from '../../assets/images/staff_upsidedown.png';
+
 import './landing.css';
 
 function Landing() {
@@ -48,9 +23,13 @@ function Landing() {
       </div>
       <section className="images-section">
         <div className="container">
-          <img src="/images/image1.jpg" alt="Image 1" className="image"/>
-          <img src="/images/image2.jpg" alt="Image 2" className="image"/>
-          <img src="/images/image3.jpg" alt="Image 3" className="image"/>
+        <img src={ScrollImage} alt="Scroll" className="scroll-image" />
+        <img src={Book} alt="Book" className="book-image" />
+        <img src={Bag} alt="Bag" className="bag-image" />
+        <img src={CoinPurse} alt="CoinPurse" className="coinpurse-image" />
+        <img src={Map} alt="Map" className="map-image" />
+        <img src={UprightStaff} alt="StaffUpright" className="uprightstaff-image" />
+        <img src={UpsideDownStaff} alt="Upsidedownstaff" className="upsidedownstaff-image" />
         </div>
       </section>
       <footer className="footer">
