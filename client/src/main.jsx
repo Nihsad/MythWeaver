1405ec8f99371b5295c20f88c7f175b73f9bcdb4
HaleighElkins <152942336+HaleighElkins@users.noreply.github.com>
2024-06-02 19:50:31 -0600

    finalized for landing and mythindex

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -43,7 +43,7 @@ const router = createBrowserRouter([
                 path: '/about-us',
                 element: <AboutUs />
             }
-            // {
+            // 
             //     path: '/author-tree',
             //     element: <Tree />
             // }
