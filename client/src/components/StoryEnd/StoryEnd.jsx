7397302444f5786ccc6f6157e0b37cc23d3287bd
Nihsadd <nihsadd@gmail.com>
2024-06-03 20:52:13 -0600

    add StoryReview component

--- client/src/components/StoryEnd/StoryEnd.jsx
+++ client/src/components/StoryEnd/StoryEnd.jsx
@@ -7,10 +7,10 @@ const StoryEnd = () => {
             <div className="story-end-container">
                 <h1>Story Result</h1>
                 <p>
-                    Here's where the result of the story will appear!
+                    Here's where the result of the story will appear! Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci ac auctor augue mauris. Nisi est sit amet facilisis magna etiam tempor orci.
                 </p>
                 <div className="story-end-button">
-                    <button className="bookmark">Bookmark Story and Leave a Review!</button>
+                    <button className="bookmark" ><a href="/story-review">Bookmark Story and Leave a Review!</a></button>
                 </div>
             </div>
         </div>
